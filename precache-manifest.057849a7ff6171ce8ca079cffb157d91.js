f2449e169adbce98b842504ecce16f956ed82a77
santoshpbagalkot <santoshpbagalkot@gmail.com>
2024-10-08 19:36:10 +0530

    Updates

---
+++ precache-manifest.057849a7ff6171ce8ca079cffb157d91.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bb2ea51baded0e03b973b89af4df732d",
+    "url": "/news-website/index.html"
+  },
+  {
+    "revision": "afbb44cc3989d6f08c25",
+    "url": "/news-website/static/css/2.b71989ea.chunk.css"
+  },
+  {
+    "revision": "a616fb262f4e63fabbab",
+    "url": "/news-website/static/css/main.d04238a8.chunk.css"
+  },
+  {
+    "revision": "afbb44cc3989d6f08c25",
+    "url": "/news-website/static/js/2.cc1f8c06.chunk.js"
+  },
+  {
+    "revision": "2133a33f4e9282dbf343",
+    "url": "/news-website/static/js/3.c60c86f6.chunk.js"
+  },
+  {
+    "revision": "a616fb262f4e63fabbab",
+    "url": "/news-website/static/js/main.c6e7228f.chunk.js"
+  },
+  {
+    "revision": "f7c1b7ec457f1c56da00",
+    "url": "/news-website/static/js/runtime~main.5e41c329.js"
+  }
+]);
