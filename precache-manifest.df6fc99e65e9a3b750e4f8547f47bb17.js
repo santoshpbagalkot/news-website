5d0970125afd38d3aff9612a9095ccc28b7166c9
santoshpbagalkot <santoshpbagalkot@gmail.com>
2024-10-09 17:28:37 +0530

    Updates

--- precache-manifest.df6fc99e65e9a3b750e4f8547f47bb17.js
+++ precache-manifest.df6fc99e65e9a3b750e4f8547f47bb17.js
@@ -1,6 +1,6 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "84952f5cab591ef018e2b666da47e401",
+    "revision": "c4107401d7766f4d272e16d8b07b0492",
     "url": "/news-website/index.html"
   },
   {
@@ -8,7 +8,7 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/news-website/static/css/2.b71989ea.chunk.css"
   },
   {
-    "revision": "f263d56ea604735d3aba",
+    "revision": "4ced5a1dc598b7c6b4b1",
     "url": "/news-website/static/css/main.3ea9d789.chunk.css"
   },
   {
@@ -20,8 +20,8 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/news-website/static/js/3.c60c86f6.chunk.js"
   },
   {
-    "revision": "f263d56ea604735d3aba",
-    "url": "/news-website/static/js/main.5a4c8128.chunk.js"
+    "revision": "4ced5a1dc598b7c6b4b1",
+    "url": "/news-website/static/js/main.02507691.chunk.js"
   },
   {
     "revision": "f7c1b7ec457f1c56da00",
